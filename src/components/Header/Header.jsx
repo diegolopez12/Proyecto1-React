c49dc15abd21b1508878b1021b3e1a653f7a863c
JuanDiego <diegolopez119812@gmail.com>
2023-08-20 20:54:41 -0600

    Creación de carpeta de componentes y componente header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+function Header(){
+    return  <img src ="/img/header.png" alt = "Org"/>
+}  //Los componentes se basan en funciones que retornan codigo HTML 
+
+
+
+export default Header; // Header esta listo para ser importado en otros archivos 
