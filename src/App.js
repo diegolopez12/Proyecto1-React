77558b7000e9ab3f5ab5b94151a731002f161af3
JuanDiego <diegolopez119812@gmail.com>
2023-08-25 00:06:26 -0600

    Se importa letra Prata por default en index.html, se agrega componente formulario y algunos estilos.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
-
+import Formulario from './components/Formulario/Formulario';
 
 function App() {
 
@@ -9,9 +9,11 @@ function App() {
     <div>
      
       <Header/>   {/* Forma recomendada de llamar un component  */}      
-
+    <Formulario/>
        {/* En react, en el App todo codigo JS va encerrado entre {} */}
       
+
+
     </div>
   );
 }
---
+++ src/components/Formulario/Formulario.jsx
@@ -0,0 +1,19 @@
+import "./Formulario.css"
+
+
+const Formulario = () =>{
+
+    return <section className="formulario">
+
+
+       <form  className="" action="">
+        
+        <h2>Rellena el formulario para crear el colaborador</h2>
+        
+        
+        </form> 
+    </section>
+}
+
+
+export default Formulario;
